6e7cad96d5c361f3adf70b603988dca71957bf40
skmadridista <saubankhan1997@gmail.com>
2024-05-22 13:45:02 -0400

    media setup done

--- core/views.py
+++ core/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-
+from items.models import Item,Catagory
 # Create your views here.
 
 def index(request):
-    return render(request,'core/index.html')
+    items = Item.objects.filter(is_sold=False)[0:6]
+    catagories = Catagory.objects.all()
+    
+    return render(request,'core/index.html',{
+        'catagories' : catagories ,
+        'items' : items
+    })
+
+
+
