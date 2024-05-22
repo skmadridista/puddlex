c99acf15388378f4a22b4251171089201edd23d9
skmadridista <saubankhan1997@gmail.com>
2024-05-22 06:44:02 +0530

    'setup_done'

---
+++ puddlex/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path,include
+from core import urls
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('core.urls')),
+]
