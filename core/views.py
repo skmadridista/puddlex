a8db99e7cbc5a738f05311abe46a3116ff9fd61f
skmadridista <saubankhan1997@gmail.com>
2024-05-23 17:39:07 -0400

    added categories to home page.

--- core/views.py
+++ core/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
-from items.models import Item,Catagory
+from items.models import Item,Category
 # Create your views here.
 
 def index(request):
     items = Item.objects.filter(is_sold=False)[0:6]
-    catagories = Catagory.objects.all()
-    
-    return render(request,'core/index.html',{
-        'catagories' : catagories ,
-        'items' : items
+    categories = Category.objects.all()
+
+    return render(request, 'core/index.html', {
+        'categories': categories,
+        'items': items,
     })
 
 
